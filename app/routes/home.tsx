80469ed9087c9066d14b2b4c67b9dcad3d7d1841
ridddzzzi <riddhi.tech@proton.me>
2025-07-21 23:42:29 -0400

    ui done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-
+import Card from "~/components/Card";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuMate" },
@@ -13,24 +13,26 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
-    <section className='main-section'>
-      <div className='page-heading'>
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review Your Submissions And Get Instant Feedbacks!</h2>
+    <section className="main-section">
+      <div className='page-heading py-16'>
+        <h1>Your Resume’s Best Mate For Success!</h1>
+        <h2>Review Your Resumes And Get Instant Feedbacks!</h2>
 
       </div>
 
-    </section>
+   
 
     {resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map((resume) => (
-
-                <ResumeCard key={resume.id} resume={resume} />
-
-          ))}
+ <ResumeCard key={resume.id} resume={resume} />
+                 ))}
         </div>
     )}
+
+</section>
+
+
 
 
 
